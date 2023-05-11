e2df3c3be0486e397d109fcf849e1c722eb22421
gaming <saylent1@gmail.com>
2023-05-11 12:15:17 +0800

    🎨 🚧 Added UI structure [wip]

--- GitActivity/src/MainForm.java
+++ GitActivity/src/MainForm.java
@@ -1,12 +1,24 @@
 import javax.swing.*;
 
 public class MainForm extends JFrame {
+
+    /// The main UI panel.
+    private Panel uiPanel;
+
     public MainForm() {
+        // add the main UI
+        uiPanel = new Panel(this);
+
         this.pack();
-        this.setSize(500,500);
+
+        this.setSize(500,250);
+
         this.setLocationRelativeTo(null);
+
         this.setVisible(true);
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
+
 
 }
---
+++ GitActivity/src/Panel.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+// Main UI Panel
+public class Panel extends JPanel {
+
+    public Panel(JFrame mainFrame) {
+        initializeButtons();
+        mainFrame.getContentPane().add(this);
+    }
+
+
+    void initializeButtons()
+    {
+
+    }
+}
